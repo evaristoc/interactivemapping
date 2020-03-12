11a3a91a982c9a4397bf7e437afa35ad637374f3
ec <varikvi@yahoo.com>
2020-03-13 00:01:30 +0100

    dev: first routers

--- routes/collections/toursRoutes.js
+++ routes/collections/toursRoutes.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 router
 .get('/',[(req,res)=>{res.send('this is all tours')}])
-.post('/',[(req,res)=>{res.send('make a tour')}])
+//.post('/',[(req,res)=>{res.send('make a tour')}])
 .get('/:tourid',[(req,res)=>{res.send(`this is a tour ${req.params.tourid}`)}])
+//.get('/:tourid',[(req,res)=>{res.send('')}])
 
 module.exports = router;
--- routes/collections/usersRoutes.js
+++ routes/collections/usersRoutes.js
@@ -5,33 +5,68 @@ const usersHandlers = require('../../controllers/handlers/usersHandlers')
 /* GET users listing. */
 //https://www.restapitutorial.com/lessons/httpmethods.html
 router
-  /*EVERYONE*/
-  .get('/', function(req, res, next) {
-    let view = usersHandlers.findAll().then((data) => {res.send(data)}).catch((err) => {return null});
-    //res.send(`view ${view}`);
-  })
+  ///*EVERYONE*/
+  //.get('/', function(req, res, next) {
+  //  let view = usersHandlers.findAll().then((data) => {res.status(200).send(data)}).catch((err) => {return null});
+  //  //res.send(`view ${view}`);
+  //})
   /*ME*/
-  .get('/me', [(req, res)=>{res.send('get me')}])
-  .put('/me?key=value', [(req, res)=>{res.send(`update ${req.query}`)}])
-  .delete('/me', [(req, res)=>{res.send('delete')}])
+  /*
+  I can:
+  -- get my personal info (initialized when registring)
+  -- modify my personal info
+  -- delete all my info
+  */
+  .get('/me', [(req, res)=>{res.status(200).send('get me')}])
+  .put('/me', [(req, res)=>{res.status(200).json(req.query)}])
+  .delete('/me', [(req, res)=>{res.status(200).send('delete me')}])
   /*My TOURS*/
-  .get('/me/t', [(req, res)=>{res.send('get my tours')}])
-  .post('me/t', [(req, res)=>{res.send('create a tour')}])
-  .delete('/me/t', [(req, res)=>{res.send('delete my tours')}])
-  .get('/me/t/:tourid', [(req, res)=>{res.send(`get a tour ${req.params.userid}`)}])
-  .put('/me/t/:tourid?key=value', [(req, res)=>{res.send('update (put) one of my tours')}])
-  .patch('/me/t/:tourid?key=value', [(req, res)=>{res.send('update (patch) one of my tours')}])
-  .delete('/me/t/:tourid', [(req, res)=>{res.send('delete this tour')}])
+  /*
+  I can:
+  -- get all my tours
+  -- create a new tour
+  -- delete all of some of my tours at once
+  -- get the info of one of my tours
+  -- update one of my tours
+  -- delete a single tour
+  */
+  .get('/me/t', [(req, res)=>{res.status(200).send('get my tours')}])
+  .post('me/t', [(req, res)=>{res.status(200).send('create a tour')}])
+  .delete('/me/t', [(req, res)=>{res.status(200).send('delete my tours')}])
+  .get('/me/t/:tourid', [(req, res)=>{res.status(200).send(`get a tour ${req.params.userid}`)}])
+  .put('/me/t/:tourid?key=value', [(req, res)=>{res.status(200).send('update (put) one of my tours')}])
+  .patch('/me/t/:tourid?key=value', [(req, res)=>{res.status(200).send('update (patch) one of my tours')}])
+  .delete('/me/t/:tourid', [(req, res)=>{res.status(200).send('delete this tour')}])
   /*The POINTS of my TOURS*/
-  .get('/me/t/:tourid/p', [(req, res)=>{res.send('get points of my tour')}])
-  .post('/me/t/:tourid/p', [(req, res)=>{res.send('create of point for my tour')}])
-  .delete('/me/t/:tourid/p', [(req, res)=>{res.send('delete points of my tour')}])
-  .get('/me/t/:tourid/p/:pointid', [(req, res)=>{res.send('get a point of my tour')}])
-  .put('/me/t/:tourid/p/:pointid?key=value', [(req, res)=>{res.send('update (put) a point of my tour')}])
-  .patch('/me/t/:tourid/p/:pointid?key=value', [(req, res)=>{res.send('update (patch) a point of my tour')}])
-  .delete('/me/t/:tourid/p/:pointid', [(req, res)=>{res.send('delete a point of my tour')}])
+  /*
+  I can:
+  -- get all the points of a tour
+  -- create a new point of a tour
+  -- delete all or some of the points of a tour
+  -- get a single point of a tour
+  -- update a single point of a tour
+  -- delete a single point of a tour
+  */
+  .get('/me/t/:tourid/p', [(req, res)=>{res.status(200).send('get points of my tour')}])
+  .post('/me/t/:tourid/p', [(req, res)=>{res.status(200).send('create of point for my tour')}])
+  .delete('/me/t/:tourid/p', [(req, res)=>{res.status(200).send('delete points of my tour')}])
+  .get('/me/t/:tourid/p/:pointid', [(req, res)=>{res.status(200).send('get a point of my tour')}])
+  .put('/me/t/:tourid/p/:pointid?key=value', [(req, res)=>{res.status(200).send('update (put) a point of my tour')}])
+  .patch('/me/t/:tourid/p/:pointid?key=value', [(req, res)=>{res.status(200).send('update (patch) a point of my tour')}])
+  .delete('/me/t/:tourid/p/:pointid', [(req, res)=>{res.status(200).send('delete a point of my tour')}])
   /*OTHER SINGLE USERS*/
-  .get('/:userid', [(req, res)=>{res.send(`see other user ${req.params.userid}`)}])
+  /*
+  I can:
+  -- get (limited) info about a single user
+  //-- XXX get all tours of a single user
+  //-- XXX get a single tour of a single user
+  
+  To get information about tours by user, go to tours router (assume tours are more important than users)
+  Like Medium - entering to the author through the article
+  */
+  .get('/:userid', [(req, res)=>{res.status(200).send(`see other user ${req.params.userid}`)}])
+  //.get('/:userid/t', [(req, res)=>{res.status(200).send('all tours of a single user')}])
+  //.get('/:userid/t/:tourid', [(req, res)=>{res.status(200).send('a single tour of a single user')}])
 
 
 module.exports = router;
