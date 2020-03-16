056d8b5316ed1ee782cf59256ae56ec156d9c99f
ec <varikvi@yahoo.com>
2020-03-16 17:57:46 +0100

    dev: checking possible test/demo DBs

--- models/testdata/index.js
+++ models/testdata/index.js
@@ -114,14 +114,164 @@ var pointsDB = db.addCollection('points');
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
 //https://stackabuse.com/a-sqlite-tutorial-with-node-js/
 //https://www.w3resource.com/node.js/nodejs-sqlite.php
+//OBS: transactions and concurrency in sqlite
+//-- https://www.sqlite.org/cgi/src/doc/begin-concurrent/doc/begin_concurrent.md
+//   (BUT I dont have that problem because in my project it is only one owner + admin who would have writing privileges)
+
 //... plus GraphQL + Apollo + Vue.js
 //https://dev.to/jgilbertcastro/build-a-simple-blog-with-graphql-node-js-sqlite-and-vue-angular-or-reactjs-3923
 //https://dev.to/jgilbertcastro/build-a-simple-blog-with-graphql-node-js-sqlite-and-vue-angular-or-reactjs-part-2-201j
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+//const fs = require('fs');
+//var toursDBfile = fs.openSync(__dirname+'/testfiles/tours.db')
+var appsql3DB = new sqlite3.Database(path.join(__dirname,'testfiles/tours.db'), ()=>{console.log('Successfully opening tours.db'), (err)=>{console.error('An issue was found opening tours.db: ', err)}});
+
+
+appsql3DB.serialize(()=>{
+  
+  	
+  appsql3DB.run("DROP TABLE IF EXISTS lorem");
+  
+  appsql3DB.run("CREATE TABLE IF NOT EXISTS lorem (info TEXT)");
+
+  var stmt = appsql3DB.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  //appsql3DB.each("SELECT rowid AS id, info FROM lorem", (err, row)=>{
+  //    console.log(row.id + ": " + row.info);
+  //});
+  //
+});
+
+appsql3DB.close(()=>{console.log('Successfully closing tours.db')}, (err)=>{console.error('An error occurred when closing tours.db: ', err)});
+
+//appsql3DB = new sqlite3.Database(__dirname,'testfiles/tours.db');
+appsql3DB = new sqlite3.Database(path.join(__dirname,'testfiles/tours.db'), ()=>{console.log('Successfully opening tours.db'), (err)=>{console.error('An issue was found opening tours.db: ', err)}});
+
+appsql3DB.serialize(()=>{
+  //appsql3DB.run(`
+  //              CREATE TABLE usersTable (
+  //                                        //userid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  //                                        name NVARCHAR(20) NOT NULL
+  //                                        //,
+  //                                        //email NVARCHAR(20) NOT NULL,
+  //                                        //username NVARCHAR(20) NULL,
+  //                                        //passport NVARCHAR(10) NOT NULL,
+  //                                        //saltedpassport NVARCHAR(100) NOT NULL,
+  //                                        //created INTEGER NOT NULL,
+  //                                        //modified INTEGER NULL
+  //                                        )
+  //              `);
+
+  //appsql3DB.run("CREATE TABLE usersTable (name TEXT)"); //VARCHAR(20) NOT NULL
+
+  //console.log(1111, appsql3DB.status);
+  appsql3DB.run("DROP TABLE IF EXISTS usersTableTest");
+  appsql3DB.run("DROP TABLE IF EXISTS toursTableTest");
+  appsql3DB.run("DROP TABLE IF EXISTS toursbyusersTableTest");
+  appsql3DB.run("DROP INDEX IF EXISTS toursbyusersIndexTest");
+  
+  appsql3DB.run("CREATE TABLE IF NOT EXISTS usersTableTest (name VARCHAR(20) NOT NULL, " +
+                "email NVARCHAR(20) NOT NULL, " +
+                "username NVARCHAR(20) NULL, " +
+                "created INTEGER NOT NULL" +
+                ")");
+  
+  appsql3DB.run("CREATE TABLE IF NOT EXISTS toursTableTest (" +
+                "tourname VARCHAR(20) NOT NULL, " +
+                "lngCenter REAL NOT NULL, " +
+                "latCenter REAL NULL, " +
+                //https://stackoverflow.com/a/54428694
+                "points TEXT NULL, " +
+                //https://www.sqlite.org/foreignkeys.html
+                //https://www.sqlitetutorial.net/sqlite-foreign-key/
+                "trackowner NVARCHAR(20) NOT NULL, " +
+                "created INTEGER NULL, " +
+                "FOREIGN KEY(trackowner) REFERENCES usersTableTest(email) " +
+                ")");
+
+  appsql3DB.run("CREATE TABLE IF NOT EXISTS toursbyusersTableTest (" +
+                "tourowner NVARCHAR(20) NOT NULL, " +
+                "tour INTEGER NOT NULL, " +
+                "FOREIGN KEY (tourowner) REFERENCES usersTableTest(email), " +
+                "FOREIGN KEY (tour) REFERENCES toursTableTest(id) " +
+                ")");
+  
+  //https://www.tutorialspoint.com/sqlite/sqlite_indexed_by.htm
+  appsql3DB.run("CREATE INDEX toursbyusersIndexTest ON toursbyusersTableTest (tourowner, tour)")
+  
+  var stmt1 = appsql3DB.prepare("INSERT INTO usersTableTest VALUES (?,?,?,?)"); //,?,?,?,?,?
+  for (var i = 0; i < 10; i++) {
+      stmt1.run(
+               [
+                "Ipsum" + i,
+                "ipsum"+i+"@mail.com",
+                "",
+                new Date()
+                //"ipsumipsum" + i, "muspi", "12332efvmertpgertregytn9tgvmt9gfgwert458ug"+i, new Date()
+                ]
+                );
+      //stmt.run("Ipsum" + i);
+      //stmt.run("Ipsum" + i);
+      //stmt.run("Ipsum" + i);
+  }
+  stmt1.finalize();
+  
+  var stmt2 = appsql3DB.prepare("INSERT INTO toursTableTest VALUES (?,?,?,?,?,?)"); //,?,?,?,?,?
+  for (var i = 0; i < 50; i++) {
+      stmt2.run(
+               [
+                "Lorem" + i,
+                77.3,
+                88.4,
+                '',
+                //Math.floor(Math.random()*(9-0+1))+0,
+                "ipsum"+ Math.floor(Math.random()*(10)) + "@mail.com",
+                new Date()
+                //"ipsumipsum" + i, "muspi", "12332efvmertpgertregytn9tgvmt9gfgwert458ug"+i, new Date()
+                ]
+                );
+      //stmt.run("Ipsum" + i);
+      //stmt.run("Ipsum" + i);
+      //stmt.run("Ipsum" + i);
+  }
+  stmt2.finalize();
+  
+  
+  
+  appsql3DB.each("SELECT rowid AS id, name, email, created FROM usersTableTest", (err, row)=>{
+      console.log(row.id + ": " + row.name + ", " + row.email+ ", " + row.created ); //+ ", " + row.email + ", " + row.username
+  });
+  
+  appsql3DB.each("SELECT * FROM toursTableTest", (err, row)=>{
+      console.log(row); //+ ", " + row.email + ", " + row.username
+  });
+  
+  appsql3DB.each("SELECT * FROM toursbyusersTableTest", (err, row)=>{
+      console.log(row); //+ ", " + row.email + ", " + row.username
+  });
+  
+  //appsql3DB.each("SELECT rowid AS id, info FROM lorem", (err, row)=>{
+  //    console.log(row.id + ": " + row.info);
+  //});
+  //console.log(2222);
+  
+});
+
+appsql3DB.close(()=>{console.log('Successfully closing tours.db')}, (err)=>{console.error('An error occurred when closing tours.db: ', err)});
+
 
 /**************/
 /*** better-sqlite3 ***/
 /**************/
-//https://github.com/JoshuaWise/better-sqlite3/blob/master/docs/api.md
+/* DIDNT INSTALL; POOR DOCS */
+//--https://github.com/JoshuaWise/better-sqlite3/blob/master/docs/api.md
+//const bsql3DB = require('better-sqlite3');
+//const appbsql3DB = new bsql3DB('toursDB.db', { verbose: console.log });
 
 
 
